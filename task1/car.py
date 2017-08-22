9716f415d8918fc7c144164b8081ddf26fa4a8b0
mrwe <erikwik@stud.ntnu.no>
2017-08-22 19:27:29 +0200

    now with more gui

--- task1/car.py
+++ task1/car.py
@@ -21,36 +21,36 @@ class Car:
   def is_valid_move(self, move, board):
      #1. Only move one space at a time
      if not (move == 1 or move == -1):
-         print 'Can only move one space at a time!'
+         print('Can only move one space at a time!')
          return False
      #2. Check that move will return a position within given indices
      if self.O == 0:
          if self.X + move > 5 or self.X + move < 0:
-             print 'Cannot move outside given X axis'
+             print('Cannot move outside given X axis')
              return False
      if self.O == 1:
          if self.Y + move > 5 or self.Y + move < 0:
-             print 'Cannot move outside given Y axis'
+             print('Cannot move outside given Y axis')
              return False
 
      #3. Check that move will not leave us in a position where another car is located
      if self.O == 0:
          if move == 1:
              if not board.boardArray[self.Y][self.X + self.S] == '-':
-                 print 'Car', board.boardArray[self.Y][self.X + self.S], 'is already at spot', self.Y, self.X + move
+                 print('Car', board.boardArray[self.Y][self.X + self.S], 'is already at spot', self.Y, self.X + move)
                  return False
          else:
              if not board.boardArray[self.Y][self.X + move] == '-':
-                 print 'Car', board.boardArray[self.Y][self.Y + move], 'is already at spot', self.Y, self.X + move
+                 print('Car', board.boardArray[self.Y][self.Y + move], 'is already at spot', self.Y, self.X + move)
                  return False
      if self.O == 1:
          if move == 1:
              if not board.boardArray[self.Y + self.S][self.X] == '-':
-                 print 'Car', board.boardArray[self.Y + self.S][self.X], 'is already at spot', self.Y + self.S, self.X
+                 print('Car', board.boardArray[self.Y + self.S][self.X], 'is already at spot', self.Y + self.S, self.X)
                  return False
          else:
              if not board.boardArray[self.Y + move][self.X] == '-':
-                 print 'Car', board.boardArray[self.Y + move][self.X], 'is already at spot', self.Y + move, self.X
+                 print('Car', board.boardArray[self.Y + move][self.X], 'is already at spot', self.Y + move, self.X)
                  return False
 
      return True
