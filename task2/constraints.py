8f915ba7a9ff05c21d0a5a7891598de2574322ca
agnete <agnetedjupvik@gmail.com>
2017-09-05 13:56:16 +0200

    Add constraints

---
+++ task2/constraints.py
@@ -0,0 +1,10 @@
+rows = []
+
+def generate_row_states(row, length):
+    for entry in row:
+        if entry == length:
+            return [entry]
+        # 1, 2, 1
+
+def is_valid_row_state(row):
+    pass
--- task2/main.py
+++ task2/main.py
@@ -1,6 +1,7 @@
 import csp
 import time
 import sys
+import constraints
 
 #Write only filename to choose board
 if len(sys.argv) > 1:
@@ -13,6 +14,8 @@ def main():
   #initialize empty board
   rows, columns = read_board(in_file)
   hitlers_final_solution = csp.CSP(rows, columns)
+  print(rows)
+  print(constraints.generate_row_states(rows[1], 6))
   return rows, columns
 
 def read_board(name):
@@ -22,7 +25,15 @@ def read_board(name):
     string_board += line
   l = string_board.split('\n')
   rows = l[1:int(l[0].split(" ")[1])+1]
+  rows = [row.split(" ") for row in rows]
+  for j in range(len(rows)):
+      for i in range(len(rows[j])):
+          rows[j][i] = int(rows[j][i])
   columns = l[int(l[0].split(" ")[1])+1:]
+  columns = [col.split(" ") for col in columns]
+  for j in range(len(columns)):
+    for i in range(len(columns[j])):
+      columns[j][i] = int(columns[j][i])
   #list(reversed(rows)) Do we need this?
   return rows, columns
 
