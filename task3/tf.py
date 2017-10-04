bf93799fe140629ec9dcf0d4b715e292d4ac05fd
agnete <agnetedjupvik@gmail.com>
2017-10-04 15:05:51 +0200

    Learny learny

--- task3/tf.py
+++ task3/tf.py
@@ -10,7 +10,7 @@ def load_data(file):
     labels = []
     for line in f.readlines():
         features.append(line.split(',')[:-1])
-        labels.append(line.split(',')[-1].strip())
+        labels.append(TFT.int_to_one_hot(int(line.split(',')[-1].strip()), 9))
 
     return np.array(features), np.array(labels)
 
@@ -22,30 +22,32 @@ def quickrun(operators, grabbed_vars=None, dir='probeview', session=None, feed_d
         TFT.show_results(results[1], grabbed_vars, dir)
     return results[0], results[1], sess
 
-def gradient_descent(filename, size_features=9, size_labels=7, steps=50, tvect=None, learning_rate=0.5, showint=10):
+def gradient_descent(filename, size_features=9, size_labels=7, steps=1000, tvect=None, learning_rate=0.5, showint=10):
     features, labels = load_data(filename)
-    print("X SHAPE")
-    print(features.shape)
-    target = tvect if tvect else labels # We have 7 different possible outputs
+    #features = np.array([features[0]])
+    print("X SHAPE", features.shape)
 
-    w = tf.Variable(np.random.uniform(-0.1, 0.1, size=(214, 214)), name='weights') # We will train these #None = 214
+    target = tvect if tvect else np.array(labels[0]) # We have 7 different possible outputs
+
+    w = tf.Variable(np.random.uniform(-0.1, 0.1, size=(size_features, size_features)), name='weights') # We will train these #None = 214
     print("W SHAPE:", w.shape)
-    b = tf.Variable(np.zeros((1, 214)), name='bias')
+    b = tf.Variable(np.zeros((1, size_features)), name='bias')
 
-    x = tf.placeholder(tf.float64, shape=(214, 214), name='input')
+    x = tf.placeholder(tf.float64, shape=(214, size_features), name='input')
     print("X SHAPE:", x.shape)
-    y = tf.sigmoid(tf.matmul(w, x) + b, name='out-sigmoid')
+    y = tf.sigmoid(tf.matmul(x, w) + b, name='out-sigmoid')
 
     error = tf.reduce_mean(tf.square(target - y))
 
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate) #ikke tenk
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
     training_operator = optimizer.minimize(error)
 
-    feeder = {x: features} #this is our data lol
+    feeder = {x: features} #this is our data
 
     sess = TFT.gen_initialized_session()
     for step in range(steps):
-        quickrun([training_operator], [w, b, y], session=sess, feed_dict=feeder, step=step, show_interval=showint)
+
+        quickrun([training_operator], [w, b, y, error], session=sess, feed_dict=feeder, step=step, show_interval=showint)
     TFT.close_session(sess)
 
 print(gradient_descent('data_sets/glass.txt'))
